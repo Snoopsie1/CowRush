ce7864c598b5f3b44b48d4cd5d27bf03493164b9
Freddiiy <frederikgaller@live.dk>
2022-11-09 11:55:13 +0100

    ajbhfk<jndwKLJKEHADWBNMKASBJF MNS,LKCJANM,LVJX HDFKEN2 M,DKCJLXHDBWMNS
    ,DKLÆEFJEB3NM,FSJKWN213JKELDGVQJD

--- Assets/Prefabs/PlayerController/PlayerController.cs
+++ Assets/Prefabs/PlayerController/PlayerController.cs
@@ -6,14 +6,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public CharacterController controller;
-    private float _speed = 5f;
-    private float _gravity = -10f;
-    private float _jumpHeight = 4f;
-
-    public Transform groundCheck;
-    private float _groundDistance = 0.4f;
-    public LayerMask groundMask;
+    private static float _initialSpeed = 5f;
+    private float _speed = _initialSpeed;
+    private float _maxSpeed = 2f;
+    private float _gravity = -30f;
     
+    private float _jumpForce = 2f;
+    private float _jumpTime = 0.33f;
+    private float _jumpTimeCounter;
+    private bool _isJumping;
+    
+    
+    
+    public Transform groundCheck;
+    private float _groundDistance = 1.11f;
+    public LayerMask groundMask;
+
+    private bool _isJumpDown = false;
     private Vector3 _velocity;
     private bool _isGrounded; 
 
@@ -25,43 +34,44 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _isGrounded = Physics.CheckSphere(groundCheck.position, _groundDistance, groundMask);
-        if (_isGrounded && _velocity.y < 0f)
-        {
-            _velocity.y = -2f;
-        }
-
+        
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        move = Vector3.ClampMagnitude(move, 1f);
+        Vector3 move = transform.right * (x * 2) + transform.forward * (z * 2);
+        move = Vector3.ClampMagnitude(move, _maxSpeed);
         
-        controller.Move(move * _speed * Time.deltaTime);
+        controller.Move(move * (_speed * Time.deltaTime));
         _velocity.y += _gravity * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && _isGrounded)
         {
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2f * _gravity);
+            _isJumping = true;
+            _jumpTimeCounter = _jumpTime;
+            _velocity = Vector3.up * _jumpForce;
+            _velocity.y = Mathf.Sqrt(_jumpForce * -2f * _gravity);
+        }
+
+        if (Input.GetButton("Jump") && _isJumping)
+        {
+            if (_jumpTimeCounter > 0)
+            {
+                _velocity = Vector3.up * _jumpForce;
+                _velocity.y = Mathf.Sqrt(_jumpForce * -2f * _gravity);
+                _jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                _isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            _isJumping = false;
         }
 
         controller.Move(_velocity * Time.deltaTime);
     }
-
-    /*
-    void CheckGrounded()
-    {
-        Physics.Raycast(transform.position, -Vector3.up, out RaycastHit hitGround);
-        float distanceToGround = hitGround.distance;
-        if (hitGround.collider == null)
-        {
-            _isGrounded = false;
-        }
-        else
-        {
-            _isGrounded = distanceToGround <= _allowJumpDistance;
-        }
-
-    }
-    */
 }
