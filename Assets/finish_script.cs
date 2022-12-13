ad4b3eaccf7065def37b79682b5030b0ffc495f2
Snoopsie1 <78692409+Snoopsie1@users.noreply.github.com>
2022-12-13 14:31:46 +0100

    Finish Script - Moved UI scenes to Scenes dir

---
+++ Assets/finish_script.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class finish_script : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject finish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player"); 
+        finish = GameObject.FindWithTag("Finish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.z >= finish.transform.position.z)
+       {
+            SceneManager.LoadScene(0);
+       }
+    }
+}
